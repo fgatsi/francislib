e00f22f35215e82d7dc6087fce1a3ac9536e49f0
Francis Gatsi <fgatsi@nd.edu>
2025-06-11 15:05:37 -0400

    Modified irsensor module

--- irsensor.py
+++ irsensor.py
@@ -4,39 +4,7 @@ from w1thermsensor import W1ThermSensor, Sensor, Unit
 from w1thermsensor.errors import SensorNotReadyError, NoSensorFoundError
 
 
-def get_state2():
-    # Open GPIO chip
-    h = lgpio.gpiochip_open(0)
-
-    ledPin = 5  # BCM 5 = physical pin 29
-
-    # Set GPIO as output
-    lgpio.gpio_claim_output(h, ledPin)
-
-    try:
-        if heat_on:
-            lgpio.gpio_write(h, ledPin, 1)  # HIGH
-        else:
-            lgpio.gpio_write(h, ledPin, 0)  # LOW
-    except KeyboardInterrupt:
-        print("Heating interrupted.")
-        lgpio.gpio_write(h, ledPin, 0)  
-    finally:
-        lgpio.gpiochip_close(h)
-
-
-def get_state():
-    
-    sensor = InputDevice(17)
-    
-    while True:
-        if sensor.is_active:
-            print("No obstacle detected")
-        else:
-            print("Obstacle detected")
-        time.sleep(0.5)
-
-def get_stateh():
+def get_state_save():
     # Constants
     GPIO_CHIP = 0         # Default chip index
     GPIO_PIN = 17         # BCM GPIO number
@@ -64,26 +32,26 @@ def get_stateh():
         lgpio.gpiochip_close(h)
 
 
-
-def get_state3():
+def get_state():
     # Constants
     GPIO_CHIP = 0         # Default chip index
-    GPIO_PIN = 17         # BCM GPIO number
-    POLL_INTERVAL = 0.5   # Seconds
+    GPIO_PIN = 17         # BCM GPIO number (pin 11 in BOARD mode)
+    POLL_INTERVAL = 1.0   # Seconds
 
-    # Open a handle to the GPIO chip
+    # Open a handle to the GPIO chip defined
     h = lgpio.gpiochip_open(GPIO_CHIP)
 
-    # Configure the pin as input
+    # Configure the pin as input to read from the sensor
     lgpio.gpio_claim_input(h, GPIO_PIN)
 
     try:
-        level = lgpio.gpio_read(h, GPIO_PIN)
+        level = lgpio.gpio_read(h, GPIO_PIN) 
+        # Note: The sensor reads '1' when there is no obstacle and '0' when one is detected
         if level == 1:
             print("No obstacle detected")  # Sensor not triggered
         else:
             print("Obstacle detected")     # Sensor triggered
-        return level
+        return ~level &  # Invert the level to get a HIGH when object is detetcted
 
     except KeyboardInterrupt:
         print("Stopped by user.")
